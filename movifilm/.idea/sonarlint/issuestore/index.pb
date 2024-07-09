75626e4e4c39e6cd1166b3f3b0db2cfd6e8149d3
nguyenkhanhlinh452003 <148494316+nguyenkhanhlinh452003@users.noreply.github.com>
2024-07-09 10:43:58 +0700

    add

---
+++ movifilm/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,15 @@
+
+9
+	README.md,8\e\8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+L
+src/components/BreadCrumb.js,a\d\ad3d7b6cdfa3e5152a2f5a3b5046ba0e91bd6cf4
+A
+public/index.html,f\4\f43ecdf8d666ee908aacce037b4cd72159e4741b
+N
+src/components/AdminSidebar.js,0\a\0ab830262b3f8135051585cb0bb5390dc058c7d4
+M
+src/components/MovieDetail.js,a\7\a70a065abeb74586749f0d40664450a338ec0fe4
+H
+src/components/Filter.js,5\e\5e84959c58c1ae2756c769d2be9fe67ef317048e
+H
+src/components/Search.js,1\6\16966eb99737e0e7030146f4c0a7b5a9b3e321e0
--- movifilm/src/components/AdminMovieDetail.js
+++ movifilm/src/components/AdminMovieDetail.js
@@ -1,49 +1,120 @@
 import axios from "axios";
+import { useEffect } from "react";
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useParams } from "react-router-dom";
 
-function AdminAddMovie() {
-  const navigate = useNavigate();
-  const [movieDetail, setMovieDetail] = useState({
-    name: "",
-    status: true,
-    description: "",
-    release_year: "",
-    director: "",
-    img_url: "",
-    trailers_url: "",
-  });
+function AdminMovieDetail() {
+  const [movieDetailOrigin, setMovieDetailOrigin] = useState({});
+  const [movieDetailChange, setMovieDetailChange] = useState({});
+  const [movieGenres, setMovieGenres] = useState([]);
+  const [genres, setGenres] = useState([]);
+  const [saveChangeMsg, setSaveChangeMsg] = useState("");
+  const [addGenreErrorMsg, setAddGenreErrorMsg] = useState("");
+  const { movieId } = useParams();
+  const [selectGenreValue, setSelectGenreValue] = useState(1);
 
-  const onChangeMovieDetail = (prop, value) => {
+  useEffect(() => {
+    fetchMovieGenre();
+  }, []);
+
+  const fetchMovieGenre = () => {
+    axios
+      // fetch all genre_id by movie_id
+      .get(`http://localhost:9999/movie_genre?movie_id=${movieId}`)
+      .then((response) => response.data)
+      .then((dataF) => {
+        const query = dataF.reduce(
+          (query, curItem) => query + "id=" + curItem.genre_id + "&",
+          ""
+        );
+        // fetch all genre item by genre_id
+        axios
+          .get(`http://localhost:9999/genre?${query}`)
+          .then((response) => response.data)
+          .then((data) => {
+            setMovieGenres(
+              dataF.map((item) => ({
+                ...item,
+                genreName: data?.find(
+                  (itemGenre) => itemGenre.id === item.genre_id
+                )?.name,
+              }))
+            );
+          });
+      });
+  };
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:9999/movie/${movieId}`)
+      .then((response) => response.data)
+      .then((data) => {
+        setMovieDetailOrigin(data);
+        setMovieDetailChange(data);
+      });
+  }, []);
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:9999/genre`)
+      .then((response) => response.data)
+      .then((data) => {
+        setGenres(data);
+      });
+  }, []);
+
+  const onChangeMovieDetailChange = (prop, value) => {
     // Create a new object by spreading the existing state
-    const updatedMovieDetail = { ...movieDetail };
+    const updatedMovieDetailChange = { ...movieDetailChange };
     // Update the property
-    updatedMovieDetail[prop] = value;
+    updatedMovieDetailChange[prop] = value;
     // Set the updated object as the new state
-    setMovieDetail(updatedMovieDetail);
+    setMovieDetailChange(updatedMovieDetailChange);
   };
 
   const handleSaveChange = () => {
     axios
-      .post(`http://localhost:9999/movie`, movieDetail)
+      .put(`http://localhost:9999/movie/${movieId}`, movieDetailChange)
       .then((response) => response.data)
       .then((data) => {
-        navigate(`/admin/movie/${data.id}`);
+        setMovieDetailOrigin(data);
+        setMovieDetailChange(data);
+        setSaveChangeMsg("Save Change success full");
+        setTimeout(() => setSaveChangeMsg(""), 3000);
       });
   };
 
   const handleDiscardChange = () => {
     const isConfirm = window.confirm("Are you sure discard change");
     if (isConfirm) {
-      setMovieDetail({
-        name: "",
-        status: true,
-        description: "",
-        release_year: "",
-        director: "",
-        img_url: "",
-        trailers_url: "",
-      });
+      setMovieDetailChange(movieDetailOrigin);
+    }
+  };
+
+  const handleDeteleMovieGenre = (id) => {
+    axios.delete(`http://localhost:9999/movie_genre/${id}`).then((response) => {
+      fetchMovieGenre();
+    });
+  };
+
+  const handleAddMovieGenre = () => {
+    if (
+      selectGenreValue &&
+      !movieGenres.some((item) => item.genre_id === selectGenreValue)
+    ) {
+      axios
+        .post("http://localhost:9999/movie_genre", {
+          movie_id: Number(movieId),
+          genre_id: Number(selectGenreValue),
+        })
+        .then((response) => {
+          fetchMovieGenre();
+        });
+    } else {
+      setAddGenreErrorMsg("Genre is exits");
+      setTimeout(() => {
+        setAddGenreErrorMsg("");
+      }, 3000);
     }
   };
 
@@ -51,12 +122,21 @@ function AdminAddMovie() {
     <div id="adminMovie">
       <form className="container">
         <div className="row">
+          <div className="col-md-12">
+            {saveChangeMsg && (
+              <div class="alert alert-success fade in">
+                <strong>Success!</strong> Save Change success full
+              </div>
+            )}
+          </div>
           <div className="form-group col-md-6">
             <label>Name:</label>
             <input
-              value={movieDetail.name}
+              value={movieDetailChange.name}
               class="form-control"
-              onChange={(e) => onChangeMovieDetail("name", e.target.value)}
+              onChange={(e) =>
+                onChangeMovieDetailChange("name", e.target.value)
+              }
             ></input>
           </div>
           <div className="form-group col-md-6">
@@ -64,17 +144,17 @@ function AdminAddMovie() {
             <br></br>
             <input
               type="radio"
-              checked={movieDetail.status}
+              checked={movieDetailChange.status}
               name="status"
-              onClick={() => onChangeMovieDetail("status", true)}
+              onClick={() => onChangeMovieDetailChange("status", true)}
             />{" "}
             Active
             <input
               type="radio"
-              checked={!movieDetail.status}
+              checked={!movieDetailChange.status}
               name="status"
               style={{ marginLeft: 20 }}
-              onClick={() => onChangeMovieDetail("status", false)}
+              onClick={() => onChangeMovieDetailChange("status", false)}
             />{" "}
             Inactive
           </div>
@@ -82,20 +162,20 @@ function AdminAddMovie() {
             <label>Description:</label>
             <textarea
               onChange={(e) =>
-                onChangeMovieDetail("description", e.target.value)
+                onChangeMovieDetailChange("description", e.target.value)
               }
               class="form-control"
               rows="5"
-              value={movieDetail.description}
+              value={movieDetailChange.description}
             ></textarea>
           </div>
           <div className="form-group col-md-6">
             <label>Release Year:</label>
             <input
               class="form-control"
-              value={movieDetail.release_year}
+              value={movieDetailChange.release_year}
               onChange={(e) =>
-                onChangeMovieDetail("release_year", e.target.value)
+                onChangeMovieDetailChange("release_year", e.target.value)
               }
             ></input>
           </div>
@@ -103,21 +183,29 @@ function AdminAddMovie() {
             <label>Director:</label>
             <input
               class="form-control"
-              value={movieDetail.director}
-              onChange={(e) => onChangeMovieDetail("director", e.target.value)}
+              value={movieDetailChange.director}
+              onChange={(e) =>
+                onChangeMovieDetailChange("director", e.target.value)
+              }
             ></input>
           </div>
           <div className="form-group col-md-6">
             <label>
               Img URL:{" "}
-              <a target="_blank" rel="noreferrer" href={movieDetail.img_url}>
+              <a
+                target="_blank"
+                rel="noreferrer"
+                href={movieDetailChange.img_url}
+              >
                 <i className="fa fa-eye"></i>
               </a>
             </label>
             <input
               class="form-control"
-              value={movieDetail.img_url}
-              onChange={(e) => onChangeMovieDetail("img_url", e.target.value)}
+              value={movieDetailChange.img_url}
+              onChange={(e) =>
+                onChangeMovieDetailChange("img_url", e.target.value)
+              }
             ></input>
           </div>
           <div className="form-group col-md-6">
@@ -126,16 +214,16 @@ function AdminAddMovie() {
               <a
                 target="_blank"
                 rel="noreferrer"
-                href={movieDetail.trailers_url}
+                href={movieDetailChange.trailers_url}
               >
                 <i className="fa fa-eye"></i>
               </a>
             </label>
             <input
               class="form-control"
-              value={movieDetail.trailers_url}
+              value={movieDetailChange.trailers_url}
               onChange={(e) =>
-                onChangeMovieDetail("trailers_url", e.target.value)
+                onChangeMovieDetailChange("trailers_url", e.target.value)
               }
             ></input>
           </div>
@@ -145,7 +233,7 @@ function AdminAddMovie() {
               type="button"
               className="btn btn-primary"
             >
-              Add Movie
+              Save Change
             </button>
             <button
               type="button"
@@ -155,10 +243,53 @@ function AdminAddMovie() {
               Discard Change
             </button>
           </div>
+          <div className="form-group col-md-6" style={{ marginTop: 50 }}>
+            <label>Genre:</label>
+            <p style={{ color: "black" }}>
+              {" "}
+              {console.log(movieGenres)}
+              {movieGenres?.map((item, index) => (
+                <span key={item.id}>
+                  {item?.genreName}
+                  <i
+                    className="fa fa-close text-danger"
+                    onClick={() => handleDeteleMovieGenre(item.id)}
+                  ></i>
+                  {index < movieGenres?.length - 1 && ", "}
+                </span>
+              ))}
+            </p>
+            <select
+              class="form-control"
+              onChange={(e) => setSelectGenreValue(Number(e.target.value))}
+            >
+              {genres?.map((item) => (
+                <option
+                  selected={item.id === selectGenreValue}
+                  key={item.id}
+                  value={item.id}
+                >
+                  {item.name}
+                </option>
+              ))}
+            </select>
+            {addGenreErrorMsg && (
+              <div class="alert alert-danger fade in" style={{ margin: 0 }}>
+                <strong>Fail!</strong> {addGenreErrorMsg}
+              </div>
+            )}
+            <button
+              type="button"
+              className="btn btn-primary"
+              onClick={handleAddMovieGenre}
+            >
+              Add
+            </button>
+          </div>
         </div>
       </form>
     </div>
   );
 }
 
-export default AdminAddMovie;
+export default AdminMovieDetail;
